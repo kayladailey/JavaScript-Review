a235020087742318305f926562582fb3ef774542
Kayla Dailey <kayladailey000@gmail.com>
2019-12-05 12:08:43 -0600

    Completed Challenge 1-3 (callbacks)

--- functions/callbacks.js
+++ functions/callbacks.js
@@ -4,38 +4,43 @@ console.log('Hello, world!');
 
 // Challenge 1
 function addTwo(num) {
-
+	return num + 2
 }
 
 // To check if you've completed it, uncomment these console.logs!
-// console.log(addTwo(3));
-// console.log(addTwo(10));
+console.log(addTwo(3));
+console.log(addTwo(10));
 
 
 // Challenge 2
 function addS(word) {
-
+  return word + 's'
 }
 
 // uncomment these to check your work
-// console.log(addS('pizza'));
-// console.log(addS('bagel'));
+console.log(addS('pizza'));
+console.log(addS('bagel'));
 
 
 // Challenge 3
 function map(array, callback) {
-
+	let newArray = [];
+   for (let i =0; i< array.length; i++){
+     newArray.push(addTwo(array[i]))
+   }
+  return newArray;
 }
 
-// console.log(map([1, 2, 3], addTwo));
+
+console.log(map([1, 2, 3], addTwo));
 
 
 // Challenge 4
 function forEach(array, callback) {
-
+	callback()
 }
 
-// see for yourself if your forEach works!
+//see for yourself if your forEach works!
 
 
 //--------------------------------------------------
